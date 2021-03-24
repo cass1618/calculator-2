a61624e9bbf1db263560edd8f4f69caa1b6144fc
cass1618 <cass16180@gmail.com>
2021-03-24 16:07:17 -0700

    Create form
    co-authored-by: Michael Burton <micbur1@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
+// BUSINESS LOGIC
 function add(number1, number2) {
   return number1 + number2;
-}
-function subtract (number1, number2) {
+};
+
+function subtract(number1, number2) {
   return number1 - number2;
-}
-function mult (number1, number2) {
+};
+
+function multiply(number1, number2) {
   return number1 * number2;
-}
-function divide (number1, number2) {
+};
+
+function divide(number1, number2) {
   return number1 / number2;
-}
+};
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-
-
-
-
-
-const result  = divide(number1, number2);
-
-alert(result);
+// USER INTERFACE LOGIC
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });  
+});
